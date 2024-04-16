05a11de3083df554753a9423c416f119ae38bc0a
Sarun M <sarun.maksuanpan@krungsrinimble.com>
2024-04-16 19:32:13 +0700

    input 4 should return 4

--- fizz_buzz.go
+++ fizz_buzz.go
@@ -7,6 +7,11 @@ func fizzbuzz(n int) string {
     if n == 2 {
         return "2"
     }
-
+    if n == 3 {
+        return "Fizz"
+    }
+    if n == 4 {
+        return "4"
+    }
     return "1"
 }
